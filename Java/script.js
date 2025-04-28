4bfa59e5c7cb86037f4cff4c75b9b89b51b811d2
Vendetta1118 <vendetta111805@gmail.com>
2025-04-27 21:28:01 -0400

    fixed hamburger

--- Java/script.js
+++ Java/script.js
@@ -1,14 +1,21 @@
 const hamburger = document.querySelector(".hamburger");
-const navlinks = document.querySelector(".navbar-links");
+const navLinks = document.querySelector(".navbar-links");
+const navbarLocation = document.querySelector(".navbar-location");
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
-    navlinks.classList.toggle("active");
+    navLinks.classList.toggle("active");
 })
 
 document.querySelectorAll(".nav-links").forEach(n => n.addEventListener("click", () => {
         hamburger.classList.remove("active");
-        navlinks.classList.remove("active");
-
+        navLinks.classList.remove("active");
+        navbarLocation.classList.remove("push-down");
 }))
+
+hamburger.addEventListener("click", () => {
+    navbarLocation.classList.toggle("push-down")
+});
+
+
 
